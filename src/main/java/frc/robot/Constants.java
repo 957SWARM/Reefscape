211a031228c528b7110228378cdc667bfd58dfbf
Dillon Pyburn <thescreamingmosquito77@gmail.com>
2025-02-08 14:16:12 -0800

    Changed Encoder ID for wrist Constants to 12

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -141,7 +141,7 @@ public final class Constants {
   public static final class WristConstants {
     // CAN IDs & Ports
     public static final int MOTOR_CAN_ID = 9;
-    public static final int ENCODER_CAN_ID  = 0;
+    public static final int ENCODER_CAN_ID  = 12;
 
     // Maximums and Minimums Allowed
     public static final double MAXIMUM_VOLTAGE = 3;
